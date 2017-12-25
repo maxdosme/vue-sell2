265370508c591d224a33d53a36e3700441a14bac
maxdosme <233705835@qq.com>
2017-12-25 15:41:00 +0800

    制作商品列表

--- js/cart.js
+++ js/cart.js
@@ -1,15 +1,29 @@
 var vm = new Vue({
     el: '#app',
     data: {
-        title : 'hello Vue'
+       // 声明商品列表（数组）
+        productList: [],
+        //  声明总价格
+        totalMoney: 0
     },
     filters: {
 
     },
-    mounted: {
-        
+    mounted: function (){
+        this.cartView();
     },
     methods: {
+        cartView: function (){
+            //  定义this
+            var _this = this;
+            // 使用data.json文件(注意这里的this作用域已变化)
+            this.$http.get("data/cartData.json",{"id": 123 }).then(function (res){
+                //  商品列表接口赋值，从json中保存到productList(面向对象封装原因，使用断点:res.data.result.list)；
+                _this.productList = res.data.result.list;
 
+                //  总价格接口赋值
+                _this.totalMoney = res.data.result.totalMoney;
+            });
+        }
     }
 })
