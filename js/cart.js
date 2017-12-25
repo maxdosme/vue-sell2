722ef4bb49ba6d0ee951c9f8e043e772f89eb670
maxdosme <233705835@qq.com>
2017-12-25 16:42:37 +0800

    添加数量和价格

--- js/cart.js
+++ js/cart.js
@@ -16,6 +16,7 @@ var vm = new Vue({
         cartView: function (){
             //  定义this
             var _this = this;
+            //  使用vue-resource插件
             // 使用data.json文件(注意这里的this作用域已变化)
             this.$http.get("data/cartData.json",{"id": 123 }).then(function (res){
                 //  商品列表接口赋值，从json中保存到productList(面向对象封装原因，使用断点:res.data.result.list)；
