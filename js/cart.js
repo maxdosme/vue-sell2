4385703e52b3b9f376c537b2f585c2ca137da415
maxdosme <233705835@qq.com>
2017-12-26 11:27:33 +0800

    过滤器格式化商品价格

--- js/cart.js
+++ js/cart.js
@@ -6,18 +6,28 @@ var vm = new Vue({
         //  声明总价格
         totalMoney: 0
     },
+    //  过滤器（格式化数据）
     filters: {
-
+        //  格式化单价金额
+        formatMoney:function (val){
+            // 格式化两位小数点
+            return "￥"+val.toFixed(2);
+        }
     },
+    //  钩子函数
     mounted: function (){
-        this.cartView();
+        //  直接vm无法调用，因为mounted钩子函数替换。
+        this.$nextTick(function (){
+            // 代码保证 this.$el 在 document 中
+            vm.cartView();
+        });
     },
     methods: {
         cartView: function (){
-            //  定义this
+            //  定义this(vm)
             var _this = this;
             //  使用vue-resource插件
-            // 使用data.json文件(注意这里的this作用域已变化)
+            // 使用data.json文件(注意这里的this作用域已变化，这里的this=全局vm)
             this.$http.get("data/cartData.json",{"id": 123 }).then(function (res){
                 //  商品列表接口赋值，从json中保存到productList(面向对象封装原因，使用断点:res.data.result.list)；
                 _this.productList = res.data.result.list;
@@ -28,3 +38,7 @@ var vm = new Vue({
         }
     }
 })
+// 全局格式化总价格
+Vue.filter('money', function (val, type){
+    return "￥" + val + type;
+});
